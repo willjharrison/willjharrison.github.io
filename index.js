3d61318df8bc9b4a5e3e67e289c718f6d564918e
willjharrison <willjharri@gmail.com>
2023-03-16 11:49:42 +1000

    Update index.js

--- index.js
+++ index.js
@@ -46,3 +46,44 @@ function loadScript(url, callback) {
 }
 
 loadScript('https://unpkg.com/color-convert@2.0.1', cycleLabColor);
+
+
+function fetchPublications() {
+  const scholarId = 'zGyx5LoAAAAJ';
+  const apiUrl = `https://bibbase.org/show?bib=https://scholar.google.co.uk/citations?user=${scholarId}&exportformat=bibtex&jsonp=1`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => displayPublications(data.entries))
+    .catch(error => console.error('Error fetching publications:', error));
+}
+
+function displayPublications(publications) {
+  const sortedPublications = publications.sort((a, b) => (a.year < b.year) ? 1 : -1);
+
+  let publicationsHtml = '<h2>Publications</h2><ul>';
+
+  sortedPublications.forEach(publication => {
+    publicationsHtml += `
+      <li>
+        <strong>${publication.title}</strong><br>
+        ${publication.author}<br>
+        <em>${publication.journal}</em>, ${publication.year}
+      </li>`;
+  });
+
+  publicationsHtml += '</ul>';
+
+  document.getElementById('publications').innerHTML = publicationsHtml;
+}
+
+function init() {
+  // Initialize color cycling
+  //initColorCycling();
+
+  // Fetch and display publications
+  fetchPublications();
+
+  // Update publications list every 24 hours
+  setInterval(fetchPublications, 24 * 60 * 60 * 1000);
+}
